b893a8738f8f2e3dc81f186afecedbeff92bdb45
dairi256 <derekingham25@outlook.com>
2026-04-10 18:07:41 +0800

    Create TLE Service - Fetches data from Celestrak API to fetch TLE data which we will use in upcoming changes.

---
+++ CLIObjectTracker/Services/TLEService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace CLIObjectTracker.Services
+{
+    public class TLEService
+    {
+        private readonly HttpClient _http = new();
+
+        private const string CelestrakUrl = "https://celestrak.com/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"; // This will be the main URL to fetch TLE data
+
+        private async Task<string[]> GetTLEAsync(string name)
+        {
+            try
+            {
+                var url = string.Format(CelestrakUrl, Uri.EscapeDataString(name));
+                var response = await _http.GetStringAsync(url);
+
+                var lines = response
+                    .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(l  => l.Trim())
+                    .ToArray();
+
+                if (lines.Length < 3)
+                    throw new Exception("TLE Data incomplete or not found.");
+
+                return lines;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[TLE ERROR] {ex.Message}");
+                return Array.Empty<string>(); // Fallback in case if the response doesn't work
+            }
+        }
+    }
+}
