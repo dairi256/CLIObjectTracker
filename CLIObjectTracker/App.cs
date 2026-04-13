59755b6b111f9290a4f4bafd76ef38e2e200e60e
dairi256 <derekingham25@outlook.com>
2026-04-13 19:45:39 +0800

    Implement ParseTLEInfo in App.cs for use of new UI. - Added a new mathod, ParseTLEInfo, which will parse the TLE data from the object so it can be useful for the UI (Implemented in future commits). - Fixed an if statement in RunAsync where the if statement 'if (tle.Length < 3) would not trigger due to the number being 0.

--- CLIObjectTracker/App.cs
+++ CLIObjectTracker/App.cs
@@ -8,9 +8,11 @@ namespace CLIObjectTracker
     internal class App
     {
         private readonly TLEService _tleService;
+        private UIService _ui; // So we can access the UI class in this file
         public App()
         {
             _tleService = new TLEService();
+            _ui = new UIService();
         }
 
         public async Task RunAsync()
@@ -20,7 +22,7 @@ namespace CLIObjectTracker
 
             var tle = await _tleService.GetTLEAsync(name!);
 
-            if (tle.Length < 0)
+            if (tle.Length < 3)
             {
                 Console.WriteLine("No TLE Data found.");
                 return;
@@ -29,6 +31,27 @@ namespace CLIObjectTracker
             Console.WriteLine("\nTLE Data:");
             foreach (var line in tle)
                 Console.WriteLine(line);
+        }
+
+        private (string Name, string Epoch, TimeSpan Age) ParseTLEInfo(string[] tle)
+        {
+            string name = tle[0];
+            string line1 = tle[1];
+
+            string epochRaw = line1.Substring(18, 24);
+
+            int year = int.Parse(epochRaw.Substring(0, 2));
+            int dayOfYear = int.Parse(epochRaw.Substring(2, 3));
+            double fractionalDay = double.Parse(epochRaw.Substring(5), System.Globalization.CultureInfo.InvariantCulture);
+
+            year += (year < 57) ? 2000 : 1900;
+
+            DateTime epoch = new DateTime(year, 1, 1).AddDays(dayOfYear - 1)
+                .AddDays(fractionalDay);
+
+            TimeSpan age = DateTime.UtcNow - epoch;
+
+            return (name, epoch.ToString("yyyy-MM-dd HH:mm:ss"), age);
         }
     }
 }
